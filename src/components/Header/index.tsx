d98a7be46b4ee67ecad7fb4e5b4f7bb4d5c553a3
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-24 18:38:08 -0300

    feat: adding landing page components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import styles from "./styles.module.scss";
 
+import { SignInButton } from "../SignInButton";
+
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a>Home</a>
-          <a>Posts</a>
+          <a href="" className={styles.active}>
+            Home
+          </a>
+          <a href="">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./styles.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <h1>Hello World</h1>
+        <title>Home | ig.news</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span>world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton priceId={"10"} />
+        </section>
+        <img src="/images/avatar.svg" alt="girl Coding" />
+      </main>
     </>
   );
 }
