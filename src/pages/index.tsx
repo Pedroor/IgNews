1daab9cc0e2b102116af844119df9d89ab97fabb
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-24 10:57:27 -0300

    config: config document and import fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <h1>Hello World</h1>
+      </Head>
+    </>
+  );
 }
