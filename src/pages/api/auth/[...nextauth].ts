1328153ac428c538cb01d048f6e8c19665fa8e7c
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-25 01:39:53 -0300

    feat: adding conditional to not duplicate users

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,8 +10,29 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: "read:user",
+      scope: "read:user", // informação que quero receber
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+  },
 });
