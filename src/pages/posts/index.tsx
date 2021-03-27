beea16c1e9b4eb6a8db32d3a2a732d12c4349220
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-26 23:00:52 -0300

    feat: create styles for individual post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -58,7 +58,7 @@ export const getStaticProps: GetStaticProps = async () => {
       slug: post.uid,
       title: RichText.asText(post.data.title),
       excerpt:
-        post.data.content.find(content => content.type === "paragraph")?.text ??
+        post.data.content.find(content => content.type === "paragraph")?.text ?? // se não existir isso retorna isso
         "",
       updatedAt: new Date(post.last_publication_date).toLocaleDateString(
         "pt-BR",
