73c84462f0cd0d30e02ddcf45a13464aad4ec53c
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-26 21:11:37 -0300

    feat: implementing SSG using Prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,22 @@
 import styles from "./styles.module.scss";
-
+import { getPrismicClient } from "../../services/prismic";
+import { RichText } from "prismic-dom";
+import Prismic from "@prismicio/client";
 import Head from "next/head";
+import { GetStaticProps } from "next";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -11,47 +25,52 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="https://howow.linebr.com/minhaconta.php">
-            <time>12 de março de 2021</time>
+          {posts.map(post => (
+            <a key={post.slug} href="https://howow.linebr.com/minhaconta.php">
+              <time>{post.updatedAt}</time>
 
-            <strong>
-              Next.JS - Novidades na versão 10 e atualização do blog para
-              melhorar a performance
-            </strong>
-            <p>
-              Vamos experimentar algumas dessas melhorias e aplicar na prática
-              no blog que criamos para ir evoluindo com essa ferramenta que tem
-              revolucionado a web.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-
-            <strong>
-              Next.JS - Novidades na versão 10 e atualização do blog para
-              melhorar a performance
-            </strong>
-            <p>
-              Vamos experimentar algumas dessas melhorias e aplicar na prática
-              no blog que criamos para ir evoluindo com essa ferramenta que tem
-              revolucionado a web.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-
-            <strong>
-              Next.JS - Novidades na versão 10 e atualização do blog para
-              melhorar a performance
-            </strong>
-            <p>
-              Vamos experimentar algumas dessas melhorias e aplicar na prática
-              no blog que criamos para ir evoluindo com essa ferramenta que tem
-              revolucionado a web.
-            </p>
-          </a>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find(content => content.type === "paragraph")?.text ??
+        "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,7 @@
 import Prismic from "@prismicio/client";
 
-export function getPrismicCliete(req?: unknown) {
-  const prismic = Prismic.client(process.env.PRISMIC_ACCESS_TOKEN, {
+export function getPrismicClient(req?: unknown) {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
     req,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
